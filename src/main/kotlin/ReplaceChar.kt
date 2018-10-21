a885dfe9ac869a60c8308a663e06f3304c02e530
Alexander Gorpinich <agorpinich@iitdgroup.ru>
2018-10-21 23:27:39 +0300

    String replace by iteration

--- src/main/kotlin/ReplaceChar.kt
+++ src/main/kotlin/ReplaceChar.kt
@@ -2,13 +2,23 @@ class ReplaceChar{
 
     fun simpleByStringApi(string: String?, ch: Char) : String {
 
-        if(string == null || string.isEmpty()) return ""
+        if(string == null || string.isBlank()) return ""
 
         var res = ""
         for (c in string) {
             if(c != ch) res = res.plus(c)
         }
         return res
+    }
+
+    fun recursively(string: String, ch: Char) : String{
+        if(string.isBlank()) return ""
+
+        return if(string.contains(ch)){
+            string.substringBefore(ch) + recursively(string.substringAfter(ch), ch)
+        } else {
+            string
+        }
     }
 
 }
--- src/test/kotlin/ReplaceCharTest.kt
+++ src/test/kotlin/ReplaceCharTest.kt
@@ -4,9 +4,23 @@ import org.junit.Test
 class ReplaceCharTest {
 
     @Test
+    fun simpleByStringApiClear() {
+        val repChar = ReplaceChar()
+        assertEquals("MustBeString", repChar.simpleByStringApi("MustBeString", 'W'))
+
+    }
+
+    @Test
     fun simpleByStringApi() {
         val repChar = ReplaceChar()
         assertEquals("MustBeString", repChar.simpleByStringApi("MustBeWString", 'W'))
+
+    }
+
+    @Test
+    fun simpleMultiByStringApi() {
+        val repChar = ReplaceChar()
+        assertEquals("MustBeString", repChar.simpleByStringApi("MusWtBeWStringW", 'W'))
 
     }
 
@@ -21,6 +35,40 @@ class ReplaceCharTest {
     fun simpleByStringApiEmpty() {
         val repChar = ReplaceChar()
         assertEquals("", repChar.simpleByStringApi("", 'W'))
+
+    }
+
+    @Test
+    fun simpleByStringApiNewLine() {
+        val repChar = ReplaceChar()
+        assertEquals("", repChar.simpleByStringApi("\n", 'W'))
+
+    }
+    @Test
+    fun byRecursively() {
+        val repChar = ReplaceChar()
+        assertEquals("MustBeString", repChar.recursively("MustBeWString", 'W'))
+
+    }
+
+    @Test
+    fun multiByRecursively() {
+        val repChar = ReplaceChar()
+        assertEquals("MustBeString", repChar.recursively("MWustBeWStringW", 'W'))
+
+    }
+
+    @Test
+    fun byRecursivelyEmpty() {
+        val repChar = ReplaceChar()
+        assertEquals("", repChar.recursively("", 'W'))
+
+    }
+
+    @Test
+    fun byRecursivelyNewLine() {
+        val repChar = ReplaceChar()
+        assertEquals("", repChar.recursively("\n", 'W'))
 
     }
 }
